25de5b78f99b383004d30555fbca57202d61a6b2
Alfonso Filho <alfonsocfilho@gmail.com>
2020-07-04 22:29:27 +0100

    doc life cicle

--- index.mjs
+++ index.mjs
@@ -83,9 +83,11 @@ const myApp = new ActorSystem({
   root: "./root.mjs",
   debug: true,
 });
+
+
 /*
 
-Methods:
+Methods: done
 - spawn
 - link
 - become
@@ -93,30 +95,37 @@ Methods:
 - send/tell
 - ask
 
-Messages:
-- start
-- stop
-- down
-- crash
-- kill
-- restart
-- died
-- info
-- unknown
+Built-in Messages types:
+
+- spawn     : create new actor
+- start     : run start handler
+- started   : reply to spawn when start is done
+
+- stop      : run stop handler and destroy actor
+- stopped   : notify linked actors when stop done
+
+- restart   : clean up actor state
+- restarted : notify linked actors when restart is done
+
+- reply     : reply ask messages
+
+- info      : get actor status, state, data
+
+- crash     : notify linked actors when actor crash
+
+- terminate : destroy actor
+- died      : notify linked actors when termination is done
+
+
+
 
 Components
 - Actor
 - ActorSystem
 
-
 Support:
 Node
 Deno
 Browser
-
-
-
-
-
 
 */
